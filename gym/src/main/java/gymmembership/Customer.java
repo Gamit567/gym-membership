fbb61097de3d264aedbd7aae4075808b16e21826
Jamal Hassan <ZNAC510>
2025-12-17 10:51:02 +0000

    #2 adding model clasess

--- gym/src/main/java/gymmembership/Customer.java
+++ gym/src/main/java/gymmembership/Customer.java
@@ -1,10 +1,22 @@
 package gymmembership;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
 public class Customer {
-	
+	@Id
+	@GeneratedValue
 	Long id;
 	boolean status;
 	String membership_type;
+	
+	@OneToOne
+	@JoinColumn(name = "user_id")
+	User user;
+	
+	
 	//getters
 	Long getId() {
 		return id;
