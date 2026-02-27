4abb3b9d9e8d49506073c0b9abbf507e553455ee
Gamit567 <kepler21222@gmail.com>
2026-02-27 12:33:05 +0000

    #2 finising customer.java

--- membership/src/main/java/com/gym/membership/model/customer.java
+++ membership/src/main/java/com/gym/membership/model/customer.java
@@ -1,7 +1,37 @@
+package com.gym.membership.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 class customer{
 
+	@Id
+	@GeneratedValue
     private Long id;
 
     private String name;
-    private int age
+    
+    private int age;
+    
+    public customer() {}
+    
+    public customer(String name, int age) {
+    	this.name = name;
+    	this.age = age;
+    }
+    
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return this.age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
